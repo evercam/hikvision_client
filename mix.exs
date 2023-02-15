1111a8d2ea968b40e1963bdb178ee05717800af5
Billal GHILAS <bilal@evercam.io>
2023-02-15 11:41:22 +0100

    Add ex_doc package

--- mix.exs
+++ mix.exs
@@ -1,13 +1,33 @@
 defmodule HikvisionClient.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @repo "https://github.com/evercam/hikvision_client"
+
   def project do
     [
       app: :hikvision_client,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      #Docs
+      name: "HikvisionClient",
+      source_url: @repo,
+      description: "Hikvision ISAPI client",
+      docs: [
+        main: "Hikivision",
+        source_ref: "v#{@version}",
+        source_url: @repo,
+        extras: ["README.md"]
+      ],
+      package: [
+        licenses: ["MIT"],
+        links: %{
+          "Github" => @repo
+        }
+      ]
     ]
   end
 
@@ -23,7 +43,8 @@ defmodule HikvisionClient.MixProject do
       {:uuid, "~> 1.1"},
       {:sweet_xml, ">= 0.0.0", optional: true},
       {:bypass, "~> 2.1", only: :test},
-      {:jason, "~> 1.4", only: :test}
+      {:jason, "~> 1.4", only: :test},
+      {:ex_doc, "~> 0.27", only: :dev, runtime: false},
     ]
   end
 end
